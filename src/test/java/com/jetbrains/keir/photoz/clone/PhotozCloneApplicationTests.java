4b77e9a8887457ee2eeb2dc6736d0982549f493b
Keir Whitlock <keir@whitlock.it>
2022-11-23 18:39:21 +0000

    Initial CRUD

---
+++ src/test/java/com/jetbrains/keir/photoz/clone/PhotozCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetbrains.keir.photoz.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotozCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
